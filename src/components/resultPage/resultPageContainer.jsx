66d4519c7366dfa6885c72a2376e59a7b05728e0
SergeyKosovich <kosovichsergey322@gmail.com>
2022-01-03 14:30:29 +0300

    add form to home page

--- src/components/resultPage/resultPageContainer.jsx
+++ src/components/resultPage/resultPageContainer.jsx
@@ -1,6 +1,11 @@
 import { React } from 'react';
+import { Container } from 'react-bootstrap';
 
 const ResultPageContainer = () => {
-  return <>Result page</>;
+  return (
+    <Container>
+      <h2 className='mt-5'>Results</h2>
+    </Container>
+  );
 };
 export default ResultPageContainer;
